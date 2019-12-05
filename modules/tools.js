0edf1749e6bbe43939b4908f6638fbf6242c5b8c
Melissa Geels <spansjh@gmail.com>
2019-12-05 03:33:18 +0100

    Fix capitalization

--- modules/tools.js
+++ modules/tools.js
@@ -6,7 +6,7 @@ class ToolsModule
 	onCmdJoinDate(msg)
 	{
 		var joinedAt = moment(msg.member.joinedAt);
-		msg.reply("You joined this server **" + joinedAt.fromNow() + "**. (" + joinedAt.format() + ")");
+		msg.reply("you joined this server **" + joinedAt.fromNow() + "**. (" + joinedAt.format() + ")");
 	}
 }
 
