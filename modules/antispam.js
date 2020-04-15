f7c1ce9afcbd8ee1624dd1b91c1645566d3b52c0
Melissa Geels <spansjh@gmail.com>
2020-04-15 12:59:08 +0000

    Added anti-spam module

---
+++ modules/antispam.js
@@ -0,0 +1,28 @@
+var moment = require("moment");
+
+class AntiSpamModule
+{
+	constructor(config, client, bot)
+	{
+		this.config = config;
+		this.client = client;
+		this.bot = bot;
+	}
+
+	onMessage(msg, edited)
+	{
+		if (msg.content.match(/https?:\/\//i) || msg.content.match(/\.[a-z]{2,3}\//i) || msg.content.match(/(bit.ly|shorturl.at|tiny.cc)/i)) {
+			msg.guild.members.fetch(msg.author).then((member) => {
+				var hours = moment().diff(member.joinedAt, "hours");
+				if (hours < this.config.linkhours) {
+					this.bot.addLogMessage("Deleted link from " + member.toString() + " who joined " + hours + " hours ago. Deleted message:\n```" + msg.content + "```");
+					msg.delete();
+					msg.author.send("Your recent message has been automatically deleted. We do not allow new users to post links for a short while, to combat spam. Check #info for more information about the rules. If you think this message is in error, please DM one of the mods.");
+					return;
+				}
+			});
+		}
+	}
+}
+
+module.exports = AntiSpamModule;
