ccf27ac84005ee86a0d64522840c728b7f64b4b7
Melissa Geels <spansjh@gmail.com>
2020-07-25 13:09:38 +0200

    Added autoreact module

---
+++ modules/autoreact.js
@@ -0,0 +1,29 @@
+const discord = require("discord.js");
+
+class AutoReactModule
+{
+	constructor(config, client, bot)
+	{
+		this.config = config;
+		/** @type {discord.Client} */
+		this.client = client;
+		this.bot = bot;
+	}
+
+	/**
+	 * @param {discord.Message} msg
+	 * @param {Boolean} edited
+	 */
+	onMessage(msg, edited)
+	{
+		if (edited) {
+			return;
+		}
+
+		if (msg.content.match(new RegExp(this.config.match, "i"))) {
+			msg.react(this.config.emoji).catch(console.error);
+		}
+	}
+}
+
+module.exports = AutoReactModule;
