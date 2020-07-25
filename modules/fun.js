86d81837f9897bf477744360740ffb9530e8c00a
Melissa Geels <spansjh@gmail.com>
2020-07-25 14:20:37 +0200

    Added fun module

---
+++ modules/fun.js
@@ -0,0 +1,50 @@
+const discord = require("discord.js");
+const RedditRadio = require("../RedditRadio");
+
+class FunModule
+{
+	constructor(config, client, bot)
+	{
+		this.config = config;
+
+		/** @type {discord.Client} */
+		this.client = client;
+
+		/** @type {RedditRadio} */
+		this.bot = bot;
+	}
+
+	/**
+	 * @param {discord.Message} msg
+	 * @param {Boolean} edited
+	 */
+	onMessage(msg, edited)
+	{
+		if (msg.content.toLowerCase() == "good bot") {
+			msg.channel.send(msg.member.toString() + " Thanks");
+			return;
+		}
+
+		if (msg.content.toLowerCase() == "bad bot") {
+			msg.channel.send(msg.member.toString() + " I'm sorry :sob: If I did something wrong, you can report a bug! <https://github.com/codecat/reddit-radio/issues>");
+			return;
+		}
+
+		if (msg.content.toLowerCase() == "kut bot") {
+			msg.channel.send(msg.member.toString() + " nou sorry hoor");
+			return;
+		}
+
+		if (msg.content.toLowerCase().indexOf("am i the only one") != -1 && msg.member !== null) {
+			msg.channel.send(msg.member.toString() + " Probably not.");
+			return;
+		}
+
+		if (msg.content.toLowerCase().indexOf(".shrug") != -1) {
+			msg.channel.send("\xaf\\\\\\_<:headykappa:330110432209797123>\\_/\xaf");
+			return;
+		}
+	}
+}
+
+module.exports = FunModule;
