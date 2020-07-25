925a0aa5d121806404c7dc4cda4c76ec19ffb884
Melissa Geels <spansjh@gmail.com>
2020-07-25 13:12:31 +0200

    Changed antispam module to filterlink

--- modules/filterlink.js
+++ modules/filterlink.js
@@ -11,11 +11,14 @@ class AntiSpamModule
 
 	onMessage(msg, edited)
 	{
+		//TODO: Add a command .permit to allow a user to post links even inside of their 60 minute countdown
+
 		if (msg.content.match(/https?:\/\//i) || msg.content.match(/\.[a-z]{2,3}\//i) || msg.content.match(/(bit.ly|shorturl.at|tiny.cc)/i)) {
 			msg.guild.members.fetch(msg.author).then((member) => {
-				var hours = moment().diff(member.joinedAt, "hours");
-				if (hours < this.config.linkhours) {
-					this.bot.addLogMessage("Deleted link from " + member.toString() + " in " + msg.channel.toString() + " who joined " + hours + " hours ago. Deleted message:\n```" + msg.content + "```");
+				var minutes = moment().diff(member.joinedAt, "minutes");
+
+				if (minutes < (this.config.minutes || 60)) {
+					this.bot.addLogMessage("Deleted link from " + member.toString() + " in " + msg.channel.toString() + " who joined " + minutes + " minutes ago. Deleted message:\n```" + msg.content + "```");
 					msg.delete();
 					msg.author.send("Your recent message has been automatically deleted. We do not allow new users to post links for a short while, to combat spam. Check #info for more information about the rules. If you think this message is in error, please DM one of the mods.");
 					return;
