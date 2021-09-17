d72a910b8684d7d1e080fdeda4463fa3edb59f27
Melissa Geels <spansjh@gmail.com>
2021-09-17 13:26:00 +0200

    New welcome module

---
+++ modules/welcome.js
@@ -0,0 +1,35 @@
+const discord = require("discord.js");
+
+class WelcomeModule
+{
+	constructor(config, client, bot)
+	{
+		this.config = config;
+		/** @type {discord.Client} */
+		this.client = client;
+		this.bot = bot;
+	}
+
+	/**
+	 * @param {discord.GuildMember} member
+	 */
+	async onMemberJoin(member)
+	{
+		var msg = this.config.messageprefix;
+
+		var msgIndex = Math.floor(Math.random() * this.config.messages.length);
+		msg += ' ' + this.config.messages[msgIndex];
+		msg = msg.replace('<name>', member.toString());
+
+		var channel = await this.client.channels.fetch(this.config.channel);
+		var message = await channel.send({
+			content: msg,
+			allowedMentions: {
+				users: []
+			}
+		});
+		message.react(this.config.emoji || "👋");
+	}
+}
+
+module.exports = WelcomeModule;
