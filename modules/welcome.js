dfb723f5304cca87fc35a301e1c801826cb6e9c7
Melissa <spansjh@gmail.com>
2021-09-17 22:22:40 +0200

    Removed @ mention from welcome module because it's kinda broken

--- modules/welcome.js
+++ modules/welcome.js
@@ -19,15 +19,20 @@ class WelcomeModule
 
 		var msgIndex = Math.floor(Math.random() * this.config.messages.length);
 		msg += ' ' + this.config.messages[msgIndex];
-		msg = msg.replace('<name>', member.toString());
+		msg = msg.replace('<name>', '**' + member.user.username + '**');
+		//msg = msg.replace('<name>', member.toString());
+		//^ Commented out because mentions for newly joined members are broken on the client
 
 		var channel = await this.client.channels.fetch(this.config.channel);
+		var message = await channel.send(msg);
+		/*
 		var message = await channel.send({
 			content: msg,
 			allowedMentions: {
 				users: []
 			}
 		});
+		*/
 		message.react(this.config.emoji || "👋");
 	}
 }
